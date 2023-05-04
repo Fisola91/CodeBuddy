d84339cb42d1c1f2851ee70bbed320ad00ceb944
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-05-04 11:46:26 +0200

    chatroom validation: chatroom belongs to the user model

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,4 @@
 class Chatroom < ApplicationRecord
-  belongs_to :user_1
-  belongs_to :user_2
+  belongs_to :user_1, class_name: "User"
+  belongs_to :user_2, class_name: "User"
 end
--- spec/models/chatroom_spec.rb
+++ spec/models/chatroom_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-# RSpec.describe Chatroom, type: :model do
-
-# end
+RSpec.describe Chatroom, type: :model do
+  describe "associations" do
+    it { should belongs_to(:user_1).class_name("User") }
+  end
+end
