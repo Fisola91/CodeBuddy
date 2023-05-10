e25abc72a0253547920d29e4af622c6f5de25a85
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-05-10 13:51:28 +0200

    User can edit profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_09_111845) do
+ActiveRecord::Schema.define(version: 2023_05_09_114838) do
 
   create_table "chatrooms", force: :cascade do |t|
     t.integer "user_1_id", null: false
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2023_05_09_111845) do
     t.string "programming_languages"
     t.string "topic_interest"
     t.string "communities"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
