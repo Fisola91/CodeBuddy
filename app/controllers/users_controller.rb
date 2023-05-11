daf3c8a85e0cbbaec07dddd2c2bbf0865ffbe2b3
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-05-11 08:54:56 +0200

    Changed CSS filed to SCSS files and make the edit form editable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    binding.pry
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect to user_path(@user), notice: "Profile updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :gender, :city, :bio, :first_name, :last_name, :communities, :topic_interest, :programming_languages, :linkedin_link, :github_link)
+  end
 end
